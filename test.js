1fa11d60334b70474bc8fcb0fa059d6da36fec83
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-05-31 00:42:21 +0600

    database

---
+++ test.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/test", {
+    serverSelectionTimeoutMS: 1000,
+  })
+  .then(() => {
+    console.log("Database Connected Successfully");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
