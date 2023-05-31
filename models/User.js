e813598d9d83e09ff2f804c50b7a9817cd1709d2
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-06-01 00:45:00 +0600

    validation added

--- models/User.js
+++ models/User.js
@@ -1,13 +1,40 @@
-const { model, Schema } = require('mongoose');
+const { model, Schema } = require("mongoose");
 
 const userSchema = new Schema({
-	name: String,
-	email: String,
-	password: String,
-	roles: [String],
-	accountStatus: String,
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 10,
+  },
+  email: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+      },
+      message: (prop) => `Invalid email :${prop.value}`,
+    },
+  },
+  password: {
+    type: String,
+    minlength: [4, "Password is too short"],
+
+    required: true,
+  },
+  roles: {
+    type: [String],
+    required: true,
+    default: ["STUDENT"],
+  },
+  accountStatus: {
+    type: String,
+    enum: ["PENDING", "ACTIVE", "REJECTED"],
+    default: "PENDING",
+    required: true,
+  },
 });
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
--- server.js
+++ server.js
@@ -14,26 +14,35 @@ app.get("/", (req, res) => {
   };
   res.json(obj);
 });
-app.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ message: "Server error occured" });
+});
+app.post("/register", async (req, res, next) => {
+  const { name, email, password, accountStatus } = req.body;
   if (!name || !email || !password) {
     return res.status(400).json({ message: "Invalid Data" });
   }
 
-  let user = await User.findOne({ email });
-  if (user) {
-    return res.status(400).json({ message: "User Already Exist" });
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User Already Exist" });
+    }
+
+    user = new User({ name, email, password, accountStatus });
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    user.password = hash;
+
+    await user.save();
+
+    res.status(201).json({ message: "User Created Successfully", user });
+  } catch (e) {
+    next(e);
   }
-
-  user = new User({ name, email, password });
-
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(password, salt);
-  user.password = hash;
-
-  await user.save();
-
-  res.status(201).json({ message: "User Created Successfully", user });
 });
 
 connectDB(
