506a19e37f29b0d14e5585f20f9f259f4778fc47
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-05-31 16:58:35 +0600

    database string added

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./db");
 const app = express();
 
-app.get('/', (req, res) => {
-	const obj = {
-		name: 'Ayman',
-		email: 'ayman@example.com',
-	};
-	res.json(obj);
+app.get("/", (req, res) => {
+  const obj = {
+    name: "Ayman",
+    email: "ayman@example.com",
+  };
+  res.json(obj);
 });
 
-app.listen(4000, () => {
-	console.log("I'm listening on port 4000");
-});
+connectDB(
+  "mongodb+srv://attendance-app:Sk5PkYLj541twUqs@cluster0.48q4yql.mongodb.net/?retryWrites=true&w=majority"
+)
+  .then(() => {
+    console.log("Database connected successfully");
+    app.listen(4000, () => {
+      console.log("I'm listening on port 4000");
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+//Sk5PkYLj541twUqs
--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/test", {
-    serverSelectionTimeoutMS: 1000,
-  })
-  .then(() => {
-    console.log("Database Connected Successfully");
-  })
-  .catch((e) => {
-    console.log(e);
-  });
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/test", {
+//     serverSelectionTimeoutMS: 1000,
+//   })
+//   .then(() => {
+//     console.log("Database Connected Successfully");
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
