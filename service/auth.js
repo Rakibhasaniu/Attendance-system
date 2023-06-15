79c0990f4a6da032c29fec854e01c80a38c0507d
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-06-15 20:15:28 +0600

    change

--- service/auth.js
+++ service/auth.js
@@ -1,38 +1,38 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
-const bcrypt = require("bcryptjs");
-const { findUserByProperty, createUser } = require("./user");
+// const jwt = require("jsonwebtoken");
+// const User = require("../models/User");
+// const bcrypt = require("bcryptjs");
+// const { findUserByProperty, createUser } = require("./user");
 
-const registerService = async ({ name, email, password }) => {
-  let user = await findUserByProperty("email", email);
-  if (user) {
-    const error = new Error("User Already Exist");
-    error.status = 400;
-    throw error;
-  }
+// const registerService = async ({ name, email, password }) => {
+//   let user = await findUserByProperty("email", email);
+//   if (user) {
+//     const error = new Error("User Already Exist");
+//     error.status = 400;
+//     throw error;
+//   }
 
-  user = new User({ name, email, password, accountStatus });
+//   user = new User({ name, email, password, accountStatus });
 
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(password, salt);
+//   const salt = await bcrypt.genSalt(10);
+//   const hash = await bcrypt.hash(password, salt);
 
-  return createUser({ name, email, hash });
-};
+//   return createUser({ name, email, hash });
+// };
 
-const loginService = async () => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  // console.log(user);
-  if (!user) {
-    return res.status(400).json({ message: "Invalid credential" });
-  }
+// const loginService = async () => {
+//   const { email, password } = req.body;
+//   const user = await User.findOne({ email });
+//   // console.log(user);
+//   if (!user) {
+//     return res.status(400).json({ message: "Invalid credential" });
+//   }
 
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) {
-    return res.status(400).json({ message: "Invalid credential" });
-  }
+//   const isMatch = await bcrypt.compare(password, user.password);
+//   if (!isMatch) {
+//     return res.status(400).json({ message: "Invalid credential" });
+//   }
 
-  delete user._doc.password;
+//   delete user._doc.password;
 
-  const token = jwt.sign(user._doc, "secret-key", { expiresIn: "2h" });
-};
+//   const token = jwt.sign(user._doc, "secret-key", { expiresIn: "2h" });
+// };
